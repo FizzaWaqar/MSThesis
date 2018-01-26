7efdf44eaf88d0dcd56ca9fca88a7394c778245a
Fizza Waqar <fizzawaqar@hotmail.com>
2018-01-26 10:28:58 +0500

    updated

--- src/main/java/mPointJava/mPoint.java
+++ src/main/java/mPointJava/mPoint.java
@@ -11,21 +11,17 @@ import java.util.ArrayList;
  * Created by fizza on 1/19/2018.
  */
 public class mPoint {
-    int size;
-    int trid;
-    ArrayList<Double> x;
-    ArrayList<Double> y;
-    ArrayList<Timestamp> t;
+    int size; //mPoint size
+    int trid; //mPoint ID
+    ArrayList<Double> x; //x-coordinates
+    ArrayList<Double> y; //y-coordinates
+    ArrayList<Timestamp> t; //Timestamps
 
-    //ArrayList<Double> check= new ArrayList<Double>();
-    ArrayList<Coordinate> cords= new ArrayList<Coordinate>();
-
-
+    ArrayList<Coordinate> cords= new ArrayList<Coordinate>(); //Initializing Coordinate objects Arraylist
 
     GeometryFactory geometryFactory = new GeometryFactory();
-    public mPoint() {
-    }
 
+   //mPoint Constructor
     public mPoint(int size, int trid, ArrayList<Double> x, ArrayList<Double> y, ArrayList<Timestamp> t) {
         this.size = size;
         this.trid = trid;
@@ -34,17 +30,12 @@ public class mPoint {
         this.t = t;
     }
 
-
     public ArrayList<Timestamp> getT() {
         return t;
     }
 
     public void setT(ArrayList<Timestamp> t) {
         this.t = t;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
     }
 
     public void setTrid(int trid) {
@@ -68,35 +59,18 @@ public class mPoint {
         return trid;
     }
 
-    public ArrayList getX() {
-        return x;
-    }
 
-    public ArrayList getY() {
-        return y;
-    }
 
-    /* public ArrayList<Double> checking()
-     {
-
-         for(int i=0; i<size; i++)
-         {
-             check.add(x.get(i));
-         }
-
-        return check;
-
-     }*/
     Coordinate[] temp = new Coordinate[this.getSize()];
-    public LineString createLineString()
+    public LineString getLineString() //get lineString for each trajectory
     {
 
         for(int i=0; i<this.getSize(); i++)
         {
-            cords.add(new Coordinate(x.get(i), y.get(i)));
+            cords.add(new Coordinate(x.get(i), y.get(i))); //Populating Coordinate objects Arraylist
         }
 
-        LineString lineString = geometryFactory.createLineString(cords.toArray(temp));
+        LineString lineString = geometryFactory.createLineString(cords.toArray(temp)); //creating linestrings
         return lineString;
     }
 
--- src/main/java/mPointJava/mpointMain.java
+++ src/main/java/mPointJava/mpointMain.java
@@ -16,7 +16,7 @@ public class mpointMain {
 
     public static Timestamp dateParse(String DT)
     {
-        final List<SimpleDateFormat> dateFormats = new ArrayList<SimpleDateFormat>(3);
+        final List<SimpleDateFormat> dateFormats = new ArrayList<SimpleDateFormat>(3); //Initializing Date Format Arraylist
 
         dateFormats.add(new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss.SSS"));
         dateFormats.add(new SimpleDateFormat("yyyy-MM-dd-HH:mm"));
@@ -26,7 +26,7 @@ public class mpointMain {
         {
             try
             {
-               return new Timestamp(formatString.parse(DT).getTime());
+               return new Timestamp(formatString.parse(DT).getTime()); // returning timestamps
             }
             catch (ParseException e) {}
         }
@@ -38,31 +38,31 @@ public class mpointMain {
     public static void main(String[] args) throws FileNotFoundException, ParseException {
 
         GeometryFactory geometryFactory = new GeometryFactory();
-        String filename="src/main/resources/trips.csv";
+        String filename="src/main/resources/trips.csv"; //input csv file... schema (objectID, TrajectoryID, Xcord, Ycord, Time)
         File file=new File(filename);
         Scanner inputFile;
         try {
-            PrintStream out = new PrintStream(new File("src/main/resources/output_latest.txt"));
+            PrintStream out = new PrintStream(new File("src/main/resources/output_latest.txt")); //writing the output to text file
             System.setOut(out);
             inputFile = new Scanner(file);
             inputFile.next();
 
-            List<Integer> tid= new ArrayList<Integer>();
-            List<Double> coordx= new ArrayList<Double>();
-            List<Double> coordy= new ArrayList<Double>();
-            List<Timestamp> time=new ArrayList<Timestamp>();
+            List<Integer> tid= new ArrayList<Integer>(); //Initializing Integer Array List to store Trajectory IDs
+            List<Double> coordx= new ArrayList<Double>(); //Initializing Double Array List to store x- Coordinates
+            List<Double> coordy= new ArrayList<Double>(); //Initializing Double Array List to store y- Coordinates
+            List<Timestamp> time=new ArrayList<Timestamp>();//Initializing Timestamp List to store Time
 
-            //int i=0;
             while(inputFile.hasNext())
             {
                 String data = inputFile.next();
                 String[] values= data.split(",");
 
-                int id=Integer.parseInt(values[1]);
-                double x=Double.parseDouble(values[2]);
-                double y=Double.parseDouble(values[3]);
+                int id=Integer.parseInt(values[1]); //TrajectoryID
+                double x=Double.parseDouble(values[2]); //Xcord
+                double y=Double.parseDouble(values[3]); //Ycord
                 String DT=values[4];
 
+                //Adding to lists
                 tid.add(id);
                 coordx.add(x);
                 coordy.add(y);
@@ -72,7 +72,7 @@ public class mpointMain {
 
             }
 
-            System.out.println("Number of Instances: " + tid.size());
+            System.out.println("Number of Instances: " + tid.size()); //Number of Data Rows
 
             int max=0;
             for(int m=0; m<tid.size(); m++)
@@ -82,43 +82,44 @@ public class mpointMain {
                     max=tid.get(m);
                 }
             }
-            System.out.println("Number of Trajectories: " + max);
+            System.out.println("Number of Trajectories: " + max); // max is the total number of trajectories in the dataset
 
-            mPoint[] mpoints=new mPoint[max+1];
+            mPoint[] mpoints=new mPoint[max+1]; //initializing mpoint arraylist of the max size. each trajectory will make one mPoint object
 
             int j=0;
-            for(int m=1; m<= max; m++)
+            for(int m=1; m<= max; m++) //outerloop for all trajectories
             {
-                int count = Collections.frequency(tid,m);
+                int count = Collections.frequency(tid,m); //get size of each trajectory based on the frequency of trajectory IDs.
 
-                if (count>1)
+                if (count>1) //at least two points make a trajectory so if size is greater than 1 then create mPoint object...
                 {
-                    int start=tid.indexOf(m);
-                    int end=start+count;
+                    int start=tid.indexOf(m); //get the start of each trajectory
+                    int end=start+count; //get the end of each trajectory
 
-                    ArrayList<Double> x=new ArrayList<Double>();
-                    ArrayList<Double> y=new ArrayList<Double>();
-                    ArrayList<Timestamp> t=new ArrayList<Timestamp>();
+                    ArrayList<Double> x=new ArrayList<Double>(); //sub arrayList of x-coordinates against each trajectory
+                    ArrayList<Double> y=new ArrayList<Double>(); //sub arrayList of y-coordinates against each trajectory
+                    ArrayList<Timestamp> t=new ArrayList<Timestamp>(); //sub arrayList of timestamps against each trajectory
 
-                    for(int n=0; n<count; n++)
+                    for(int n=0; n<count; n++) //Innerloop.. size of the loop is dependent on trajectory size
                     {
+                        //adding x,y,t to sub arrays
                         x.add(coordx.subList(start, end).get(n));
                         y.add(coordy.subList(start, end).get(n));
                         t.add(time.subList(start,end).get(n));
 
                     }
 
-                    mpoints[m]=new mPoint(count,m,x,y,t);
-                    mpoints[m].setTrid(m);
-                    mpoints[m].setX(x);
-                    mpoints[m].setY(y);
-                    mpoints[m].setT(t);
+                    mpoints[m]=new mPoint(count,m,x,y,t); //creating mPoint object for each trajectory
+                    mpoints[m].setTrid(m); //TrajectoryID
+                    mpoints[m].setX(x);    // x-coordinates
+                    mpoints[m].setY(y);    //y-coordinates
+                    mpoints[m].setT(t);    //Timestamps
 
 
-                    System.out.println("TID " + mpoints[m].getTrid());
-                    System.out.println("LinStrings Size " + count);
-                    System.out.println("Timestamps of Trajectory " + m + " is " +mpoints[m].getT());
-                    System.out.println("Linestring of Trajectory ID # " + m + " is " +mpoints[m].createLineString());
+                    System.out.println("TID " + mpoints[m].getTrid()); //get Trajectory ID
+                    System.out.println("LinStrings Size " + count); //Size of the trajectory
+                    System.out.println("Timestamps of Trajectory " + m + " is " +mpoints[m].getT()); //get Timestamps of each trajectory
+                    System.out.println("Linestring of Trajectory ID # " + m + " is " +mpoints[m].getLineString()); //get Linestring of each trajectory
                     System.out.println("");
                 }
                 else{
@@ -137,41 +138,7 @@ public class mpointMain {
         {
             e.printStackTrace();
         }
-/*
 
-
-        double[] cordx={72.85630199,72.8626665,72.86699216};
-        double[] cordy={33.3697771,33.3655341,33.37024549};
-        double[] time= {1010,1025,1035};
-
-        Coordinate[] cords = new Coordinate[3];
-
-        Coordinate checkCord=new Coordinate(14.2,85.3);
-        System.out.print("Checking" + checkCord);
-
-        for(int i=0; i<3; i++)
-        {
-            cords[i]= new Coordinate(cordx[i],cordy[i]);
-
-            System.out.print(cords[i]);
-        }
-
-        System.out.println("TO STRING"+Arrays.toString(cords));
-      //  System.out.println("AS LIST"+ Arrays.asList(cords));
-      //  System.out.println("AS STREAM"+ Arrays.stream(cords));
-        LineString lineString = geometryFactory.createLineString(cords);
-        System.out.println("Linestring" + lineString);
-
-        Geometry envelope = lineString.getEnvelope();
-        System.out.print("Envelope" + envelope);
-
-        MultiLineString check= geometryFactory.createMultiLineString(new LineString[]
-        {
-            lineString
-        });
-
-
-*/
     }
 
 }
